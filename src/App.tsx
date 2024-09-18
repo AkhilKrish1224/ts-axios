e7cc67689e4c35b4a9f153d40a4e02001bd0c157
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-18 23:34:30 +0530

    Initial api request with axios and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+export type Crypto = {
+  id: string;
+  name: string;
+  price_usd: string;
+  symbol: string;
+  volume24: number;
+  volume24a: number;
+};
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+  useEffect(() => {
+    const url = "https://api.coinlore.net/api/tickers/";
+    axios(url).then((response) => {
+      setCryptos(response.data.data);
+    });
+  }, []);
+  return (
+    <div className="App">
+      {cryptos
+        ? cryptos.map((crypto) => {
+            return <p>{crypto.name + " $" + crypto.price_usd}</p>;
+          })
+        : null}
+    </div>
+  );
 }
 
 export default App;
