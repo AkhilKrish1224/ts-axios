05eff5f5a60cbc6502cbf3105d2ef737092d464b
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-19 18:28:34 +0530

    Added Chartjs and Changed Type

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,82 @@ import axios from "axios";
 import CryptoSummary from "./components/CryptoSummary";
 import { Crypto } from "./Types";
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import type { ChartData, ChartOptions } from "chart.js";
+import moment from "moment";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto | null>();
+  const [data, setData] = useState<ChartData<"line">>();
+  const [options, setOptions] = useState<ChartOptions<"line">>({
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  });
   useEffect(() => {
-    const url = "https://api.coinlore.net/api/tickers/";
+    const url =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
     axios(url).then((response) => {
-      setCryptos(response.data.data);
+      setCryptos(response.data);
     });
   }, []);
+
   return (
     <>
       <div className="App">
         <select
           onChange={(e) => {
-            const c = cryptos?.find((x) => x.nameid === e.target.value);
+            const c = cryptos?.find((x) => x.id === e.target.value);
             setSelected(c);
+            axios
+              .get(
+                `https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=usd&days=30&interval=daily`
+              )
+              .then((response) => {
+                console.log(response.data);
+                setData({
+                  labels: response.data.prices.map((price: number[]) => {
+                    return moment.unix(price[0] / 1000).format("MM-DD");
+                  }),
+                  datasets: [
+                    {
+                      label: "Dataset 1",
+                      data: response.data.prices.map((price: number[]) => {
+                        return price[1];
+                      }),
+                      borderColor: "rgb(255, 99, 132)",
+                      backgroundColor: "rgba(255, 99, 132, 0.5)",
+                    },
+                  ],
+                });
+              });
           }}
           defaultValue="default"
         >
@@ -27,7 +87,7 @@ function App() {
           {cryptos
             ? cryptos.map((crypto) => {
                 return (
-                  <option key={crypto.nameid} value={crypto.nameid}>
+                  <option key={crypto.id} value={crypto.id}>
                     {crypto.name}
                   </option>
                 );
@@ -36,6 +96,11 @@ function App() {
         </select>
       </div>
       {selected ? <CryptoSummary crypto={selected} /> : null}
+      {data ? (
+        <div style={{ width: 600 }}>
+          <Line options={options} data={data} />
+        </div>
+      ) : null}
     </>
   );
 }
